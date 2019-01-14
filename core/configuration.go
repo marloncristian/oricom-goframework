23e826ded96009256af8e1ff2e29a0a814d8da04
Marlon Cristian Pereira <marloncristian@gmail.com>
2019-01-14 12:08:47 -0200

    updating configurations

--- core/configuration.go
+++ core/configuration.go
@@ -6,18 +6,8 @@ import (
 	"os"
 )
 
-// Configuration structure
-type Configuration struct {
-	Security SecurityConfiguration
-}
-
-// SecurityConfiguration security configuration struct
-type SecurityConfiguration struct {
-	Secret string
-}
-
 // GetConfiguration returns a single instance of configuration
-func GetConfiguration() (*Configuration, error) {
+func GetConfiguration(configuration *interface{}) error {
 
 	evar := os.Getenv("ORI_ENV")
 	scnf := "development"
@@ -27,17 +17,16 @@ func GetConfiguration() (*Configuration, error) {
 
 	fil, err := os.Open(fmt.Sprintf("settings.%s.json", scnf))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	cnf := &Configuration{}
-	if err := json.NewDecoder(fil).Decode(cnf); err != nil {
-		return nil, err
+	if err := json.NewDecoder(fil).Decode(configuration); err != nil {
+		return err
 	}
 
 	if err := fil.Close(); err != nil {
-		return nil, err
+		return err
 	}
 
-	return cnf, nil
+	return nil
 }
